30d9bf44b9e9bdedfeef4f2d99009d7da140c011
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-09 22:22:44 -0400

    Update relationships.py

--- relationships.py
+++ relationships.py
@@ -45,4 +45,84 @@ plt.ylabel('Weight in kg')
 plt.show()
 
 
+""" height & Weight """
+# Drop rows with missing data
+data = brfss.dropna(subset=['_HTMG10', 'WTKG3'])
 
+# Make a box plot
+sns.boxplot(x='_HTMG10', y='WTKG3', data=data, whis=10)
+
+# Plot the y-axis on a log scale
+plt.yscale('log')
+
+# Remove unneeded lines and label axes
+sns.despine(left=True, bottom=True)
+plt.xlabel('Height in cm')
+plt.ylabel('Weight in kg')
+plt.show()
+
+
+""" Distribution of Income """
+# Extract income
+income = brfss['INCOME2']
+
+# Plot the PMF
+Pmf(income).bar()
+
+# Label the axes
+plt.xlabel('Income level')
+plt.ylabel('PMF')
+plt.show()
+
+
+""" Income and height --->>> violinplot"""
+# Drop rows with missing data
+data = brfss.dropna(subset=['INCOME2', 'HTM4'])
+
+# Make a violin plot
+sns.violinplot(x='INCOME2', y='HTM4', data=data, inner=None)
+
+# Remove unneeded lines and label axes
+sns.despine(left=True, bottom=True)
+plt.xlabel('Income level')
+plt.ylabel('Height in cm')
+plt.show()
+
+
+""" Correlation """
+# Select columns
+columns = ['AGE', 'INCOME2', '_VEGESU1']
+subset = brfss[columns]
+
+# Compute the correlation matrix
+print(subset.corr())
+
+
+
+""" Simple regression--->>>linear """
+from scipy.stats import linregress
+
+# Extract the variables
+subset = brfss.dropna(subset=['INCOME2', '_VEGESU1'])
+xs = subset['INCOME2']
+ys = subset['_VEGESU1']
+
+# Compute the linear regression
+res = linregress(xs, ys)
+print(res)
+
+
+# Plot the scatter plot
+plt.clf()
+x_jitter = xs + np.random.normal(0, 0.15, len(xs))
+plt.plot(x_jitter, ys, 'o', alpha=0.2)
+
+# Plot the line of best fit
+fx = np.array([xs.min(), xs.max()])
+fy = res.intercept + res.slope * fx
+plt.plot(fx, fy, '-', alpha=0.7)
+
+plt.xlabel('Income code')
+plt.ylabel('Vegetable servings per day')
+plt.ylim([0, 6])
+plt.show()
